7e4d11d0ad27b5cf57d0b2ff8dfa72b660045ce8
lukaszkuczynski <kuczynskilukasz@gmail.com>
2021-02-08 15:24:29 +0100

    Circuit 04 program finally added

---
+++ 04_2LEDs/04_2LEDs.ino
@@ -0,0 +1,14 @@
+
+void setup() {
+  pinMode(2, OUTPUT);
+  pinMode(5, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(2, HIGH);
+  digitalWrite(5, LOW);
+  delay(1000);
+  digitalWrite(2, LOW);
+  digitalWrite(5, HIGH);
+  delay(500);
+}
