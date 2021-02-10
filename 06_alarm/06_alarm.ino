85921273bb49580094e7c5cd7ecccb3722b24025
lukaszkuczynski <kuczynskilukasz@gmail.com>
2021-02-10 17:04:42 +0100

    Alarm upgraded

--- 06_alarm/06_alarm.ino
+++ 06_alarm/06_alarm.ino
@@ -1,16 +1,21 @@
 
+const int LED_OUTPUT = 2;
+const int BUZZER_OUTPUT = 4;
+const int REED_INPUT = 21;
+
+
 void setup() {
-  pinMode(2, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(21, INPUT);
+  pinMode(LED_OUTPUT, OUTPUT);
+  pinMode(BUZZER_OUTPUT, OUTPUT);
+  pinMode(REED_INPUT, INPUT);
 }
 
 void loop() {
-  digitalWrite(2, HIGH);
-  int button_state = digitalRead(21);
+  digitalWrite(LED_OUTPUT, HIGH);
+  int button_state = digitalRead(REED_INPUT);
   int door_opened = abs(1 - button_state);
-  digitalWrite(5, door_opened);  
+  digitalWrite(BUZZER_OUTPUT, door_opened);  
   delay(100);
-  digitalWrite(2, LOW);
+  digitalWrite(LED_OUTPUT, LOW);
   delay(100);
 }
